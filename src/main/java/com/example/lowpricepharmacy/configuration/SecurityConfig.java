d43b9fc90057aa94fe813d758f54d4ca769895dd
HrisKirill <hristoforow.kirill@gmail.com>
2022-06-28 16:36:17 +0300

    [LowPricePharmacy] bugs fixed

--- src/main/java/com/example/lowpricepharmacy/configuration/SecurityConfig.java
+++ src/main/java/com/example/lowpricepharmacy/configuration/SecurityConfig.java
@@ -13,8 +13,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
                 .authorizeRequests()
                 .antMatchers("/create-short").authenticated()
                 .anyRequest().permitAll()
